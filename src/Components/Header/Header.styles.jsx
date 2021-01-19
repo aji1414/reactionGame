aee65db0d34af5d798341ee5fe4be5aac8c6b7ca
Ajibola Lawal <ajisofteng@gmail.com>
2021-01-19 19:47:02 +0000

    basic styling and responsiveness for nav bar

--- src/Components/Header/Header.styles.jsx
+++ src/Components/Header/Header.styles.jsx
@@ -1,3 +1,7 @@
 import styled from "styled-components";
 import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
  
+export const NavbarContainer = styled(Navbar)`
+background:"red !important";
+color:"blue";
+`;
